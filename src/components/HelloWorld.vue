56f5034af7bb11f6ea2ea15192be3c6369b2920a
Matheus Freitas <matheus.freitas@ibti.net.br>
2020-10-01 17:37:36 -0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <form class="body">
+    <div class="column-left">
+      <div class="column-group">
+        <label for="INSS">INSS:</label>
+        <input readonly v-model="inss" name="inss" id="INSS"/>
+      </div>
+      <div class="column-group">
+        <label for="IRRF">IRRF:</label>
+        <input readonly v-model="irrf" name="irrf" id="IRRF"/>
+      </div>
+      <div class="column-group">
+        <label for="SALARIO_COM_DESCONTOS">Total:</label>
+        <input readonly v-model="salarioComDesconto" name="salarioComDesconto" type='number' id="SALARIO_COM_DESCONTOS"/>
+      </div>
+    </div>
+
+    <div class="column-right">
+      <label for="SALARIO">SALARIO:</label>
+      <input autofocus @keyup="onKeyPress($event.target.value)" type='number' id="SALARIO"/>
+    </div>
+
+  </form>
 </template>
 
+
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    data (){
+      return {
+        inss: 0,
+        irrf: 0,
+        salarioComDesconto: 0,
+      }
+    },
+    methods: {
+      onKeyPress: function(salario){
+        console.log(salario)
+        let inssSubt = 0;
+        let irrfSubt = 0;
+        let result = 0;
+
+        if(salario <= 1045 ){
+          inssSubt = salario * 0.075
+        }else if (salario >= 1045.01 && salario <= 2089.60	) {
+          inssSubt = salario * 0.09
+        }else if (salario >= 2089.61 && salario <= 3134.40) {
+          inssSubt = salario * 0.12
+        }else if (salario >= 3134.41 && salario >= 6101.06) {
+          inssSubt = Math.round((salario * 0.14) * 100) / 100
+        }
+
+        if(salario <= 1903.98 ){
+          irrfSubt = 0
+        }else if (salario >= 1903.99 && salario <= 2826.65	) {
+          irrfSubt = salario * 0.075
+        }else if (salario >= 2826.66 && salario <= 3751.05) {
+          irrfSubt = salario * 0.15
+        }else if (salario >= 3751.06 && salario <= 4664.68) {
+          irrfSubt = salario * 0.225
+        }else if (salario >= 4664.69){
+          irrfSubt = Math.round((salario * 0.275) * 100) / 100
+        }
+
+        result = salario - irrfSubt - inssSubt;
+        this.inss = inssSubt;
+        this.irrf = irrfSubt;
+        this.salarioComDesconto = Math.round(result * 100) / 100
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.body{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.column-left{
+  display: flex;
+  flex-direction: column;
+  flex-flow: column wrap;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+.column-group{
+  align-items: flex-start;
 }
 </style>
